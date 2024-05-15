1cd661d9816a051c3563314babbe70512090ec9a
heojiu <jiwoo12021@naver.com>
2024-05-16 00:51:06 +0900

    Initial Commit

---
+++ voiceApp/voiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceAppApp.swift
+//  voiceApp
+//
+//  Created by heojiwoo on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
